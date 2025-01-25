5f874efcd075421d7e24fd824586ffd2ece37a2b
unknown <sm8004393@gmail.com>
2025-01-25 14:12:38 +0500

    Add Events and Projects Page

---
+++ .history/frontend/src/App_20250125124006.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import PageLayout from "./layouts/pageLayout";
+
+// PAGES
+import Home from "./pages/home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <PageLayout>
+            <Home />
+          </PageLayout>
+        }/>
+        <Route path="/about" element={
+          <PageLayout>
+            <About />
+          </PageLayout>
+        }/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
---
+++ .history/frontend/src/App_20250125125138.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import PageLayout from "./layouts/pageLayout";
+
+// PAGES
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Event from "./pages/Event.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PageLayout>
+              <Home />
+            </PageLayout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PageLayout>
+              <About />
+            </PageLayout>
+          }
+        />
+        <Route
+          path="/events"
+          element={
+            <PageLayout>
+              <Event />
+            </PageLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
